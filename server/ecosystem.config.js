71bf9f150b7d505f9930e4dedd751679c012c576
jeffrey-clements <freeclements@gmail.com>
2024-10-15 21:56:11 -0500

    dependency adjustment

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,20 +1,19 @@
-module.exports = {
+  module.exports = {
     apps: [
       {
         name: 'hobohippie',
-        script: './server.js',
+        script: './server/server.js',
         instances: 'max',
-        exec_mode: 'cluster',
-        watch: true,
+        exec_mode: 'cluster', 
+        watch: process.env.NODE_ENV !== 'production',
         env: {
           NODE_ENV: 'development',
-          PORT: 5000,
+          PORT: 3000,
         },
         env_production: {
           NODE_ENV: 'production',
-          PORT: 5000,
+          PORT: 3000,
         },
       },
     ],
   };
-  
