f9db73f682cd40ce242207be83370a9ad990bdb6
jeffrey-clements <freeclements@gmail.com>
2024-10-17 20:00:46 -0500

    test???

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
             </ul>
           </div>
           <div className="footer-column">
-            <h3>QuickieLinkielikeydoodoo</h3>
+            <h3>Links</h3>
             <ul>
               <li><Link to="/products">Category 1</Link></li>
               <li><Link to="/products">Category 2</Link></li>
