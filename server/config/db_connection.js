7f79a53f520dde4255d73c0caeade3c0233ccbec
jeffrey-clements <freeclements@gmail.com>
2024-10-20 18:03:33 -0500

    mongoDB

--- server/config/db_connection.js
+++ server/config/db_connection.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connectionString = process.env.MONGODB_URI;
-console.log('MongoDB URI:', connectionString);
 mongoose.set('strictQuery', false);
 
 mongoose.connect(connectionString, {
