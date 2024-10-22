ac9bbbd561e71a3a2e5af561139a62f1543f24b7
jeffrey-clements <freeclements@gmail.com>
2024-10-21 19:16:53 -0500

    pm2 ecosystem

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,19 +1,21 @@
-  module.exports = {
-    apps: [
-      {
-        name: 'Hobohippie-Backend',
-        script: './server.js',
-        instances: '1',
-        exec_mode: 'cluster', 
-        watch: process.env.NODE_ENV !== 'production',
-        env: {
-          NODE_ENV: 'development',
-          PORT: 3000,
-        },
-        env_production: {
-          NODE_ENV: 'production',
-          PORT: 3000,
-        },
+module.exports = {
+  apps: [
+    {
+      name: 'Hobohippie-Backend',
+      script: './server.js', // Entry point for your backend application
+      instances: '1', // Number of instances to run
+      exec_mode: 'cluster', // Cluster mode for load balancing
+      watch: process.env.NODE_ENV !== 'production', // Enable watch mode in development
+      env: {
+        NODE_ENV: 'development', // Environment variable for development
+        PORT: 3000, // Port for development
+        MONGODB_URI: 'mongodb+srv://free:7130TulipTrail3723542@hobohippie.rdv1r.mongodb.net/?retryWrites=true&w=majority&appName=HoboHippie', // Development MongoDB URI
       },
-    ],
-  };
+      env_production: {
+        NODE_ENV: 'production', // Environment variable for production
+        PORT: 3000, // Port for production
+        MONGODB_URI: 'mongodb+srv://free:7130TulipTrail3723542@hobohippie.rdv1r.mongodb.net/?retryWrites=true&w=majority&appName=HoboHippie', // Production MongoDB URI
+      },
+    },
+  ],
+};
