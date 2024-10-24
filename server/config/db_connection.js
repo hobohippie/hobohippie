ae620667b3c89f11279a5daf272f8ffbfdb81935
jeffrey-clements <freeclements@gmail.com>
2024-10-24 00:35:59 -0500

    mongoDB

--- server/config/db_connection.js
+++ server/config/db_connection.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 // const connectionString = process.env.MONGODB_URI;
-const connectionString = 'mongodb+srv://free:7130TulipTrail3723542@hobohippie.rdv1r.mongodb.net/?retryWrites=true&w=majority&appNçame=HoboHippie';
+const connectionString = 'mongodb+srv://free:7130TulipTrail3723542@hobohippie.rdv1r.mongodb.net/?retryWrites=true&w=majority&appName=HoboHippie';
 
 mongoose.set('strictQuery', false);
 
