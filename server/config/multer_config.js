aec4dfb870f87604d71b725cf51c9038bccb6a3c
jeffrey-clements <freeclements@gmail.com>
2024-10-26 21:56:42 -0500

    multer

--- server/config/multer_config.js
+++ server/config/multer_config.js
@@ -4,7 +4,7 @@ const path = require('path');
 // Configure storage
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/'); // Directory to store the uploaded images
+        cb(null, '../uploads'); // Directory to store the uploaded images
     },
     filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
