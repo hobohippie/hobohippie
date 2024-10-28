88ad9f726cc2a7223d3e188ee8a12984c9273bad
jeffrey-clements <freeclements@gmail.com>
2024-10-28 10:44:29 -0500

    create product

--- server/config/multer_config.js
+++ server/config/multer_config.js
@@ -1,21 +1,10 @@
 const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
-
-const uploadsDir = path.join(__dirname, '..', 'uploads');
-
-if (!fs.existsSync(uploadsDir)) {
-    fs.mkdirSync(uploadsDir, { recursive: true });
-}
-
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, uploadsDir);
+        cb(null, '/uploads');
     },
     filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        console.log('Uploading file:', file.originalname);
-        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+        cb(null, file.originalname);
     }
 });
 
