64c512bcf430a1c7005ad81c5e879b18f69e4ee4
jeffrey-clements <freeclements@gmail.com>
2024-10-28 12:48:11 -0500

    create product

--- server/config/multer_config.js
+++ server/config/multer_config.js
@@ -6,10 +6,10 @@ const storage = multer.diskStorage({
         cb(null, path.join(__dirname, '../uploads'));
     },
     filename: function (req, file, cb) {
-        console.log(file.originalname)
         const extension = path.extname(file.originalname);
         const basename = path.basename(file.originalname, extension);
         cb(null, `${basename}${extension}`);
+        console.log(`${basename}${extension}`)
     }
 });
 
--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 module.exports = {
     async createProduct(req, res) {
-        console.log(res.body)
+        console.log(req.body)
         try {
             if (!req.body.name || !req.body.price) {
                 return res.status(400).json({ message: 'Name and price are required.'});
