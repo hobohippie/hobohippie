69cd0cf98dc76d2f5edfa2ded984da196a7b6843
jeffrey-clements <freeclements@gmail.com>
2024-10-30 20:43:57 -0500

    stripe

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './main.css';
@@ -43,6 +45,8 @@ import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 
 import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
+
 const App = () => {
   return (
     <div className="app-container">
@@ -51,7 +55,7 @@ const App = () => {
           <Router>
             <ScrollToTop />
             <Header />
-            <LoginBanner/>
+            <LoginBanner />
             <div className="content-wrap">
               <Routes>
                 <Route path="/" element={<Home />} />
@@ -62,7 +66,6 @@ const App = () => {
                 <Route path="/create-account" element={<CreateAccount />} />
                 <Route path="/user-profile" element={<UserProfile />} />
                 <Route path="/wishlist" element={<Wishlist />} />
-                <Route path="/checkout" element={<Checkout />} />
                 <Route path="/order-status" element={<OrderStatus />} />
                 <Route path="/manage-subscriptions" element={<ManageSubscriptions />} />
                 <Route path="/shipping-returns" element={<ShippingReturns />} />
@@ -82,6 +85,12 @@ const App = () => {
                 <Route path="/create-product" element={<ProtectedRoute element={<CreateProduct />} />} />
                 <Route path="/edit-profile" element={<EditProfile />} />
                 <Route path="/create-supplier" element={<ProtectedRoute element={<CreateSupplier />} />} />
+                <Route path="/checkout" element={
+                  <Elements stripe={stripePromise}>
+                    <Checkout />
+                  </Elements>
+                }
+                />
                 <Route path="*" element={<NotFound />} />
               </Routes>
             </div>
