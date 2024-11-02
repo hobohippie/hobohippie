ff31f19ee0c6001168966e74f41aaac4f0bbe4db
jeffrey-clements <freeclements@gmail.com>
2024-11-02 17:08:07 -0500

    stripe

--- client/src/pages/checkout.jsx
+++ client/src/pages/checkout.jsx
@@ -15,40 +15,40 @@ function CheckoutForm() {
     return total + item.price * item.quantity;
   }, 0);
 
-  useEffect(() => {
-    const fetchClientSecret = async () => {
-      try {
-        const response = await fetch('/api/create-payment-intent', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ amount: totalAmount.toFixed(2) })
-        });
+  // useEffect(() => {
+  //   const fetchClientSecret = async () => {
+  //     try {
+  //       const response = await fetch('/api/create-payment-intent', {
+  //         method: 'POST',
+  //         headers: { 'Content-Type': 'application/json' },
+  //         body: JSON.stringify({ amount: totalAmount.toFixed(2) })
+  //       });
 
-        console.log('Response status:', response.status); // Log response status
+  //       console.log('Response status:', response.status); // Log response status
 
-        if (!response.ok) {
-          const errorMessage = await response.text();
-          throw new Error(`Failed to create payment intent. ${errorMessage}`);
-        }
+  //       if (!response.ok) {
+  //         const errorMessage = await response.text();
+  //         throw new Error(`Failed to create payment intent. ${errorMessage}`);
+  //       }
 
-        const data = await response.json();
+  //       const data = await response.json();
 
-        console.log('Data received:', data); // Log received data
+  //       console.log('Data received:', data); // Log received data
 
-        if (data.clientSecret) {
-          console.log(data.clientSecret)
-          setClientSecret(data.clientSecret);
-        } else {
-          throw new Error("Failed to get client secret from server.");
-        }
-      } catch (err) {
-        setError('Error fetching client secret. Please try again.');
-        console.error('Fetch client secret error:', err);
-      }
-    };
+  //       if (data.clientSecret) {
+  //         console.log(data.clientSecret)
+  //         setClientSecret(data.clientSecret);
+  //       } else {
+  //         throw new Error("Failed to get client secret from server.");
+  //       }
+  //     } catch (err) {
+  //       setError('Error fetching client secret. Please try again.');
+  //       console.error('Fetch client secret error:', err);
+  //     }
+  //   };
 
-    fetchClientSecret();
-  }, []);
+  //   fetchClientSecret();
+  // }, []);
 
 
 
