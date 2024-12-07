54111dbf54784c416d3a7f2831902997d146baab
Jack <jack.clements@rocketmail.com>
2024-12-07 11:42:12 -0600

    changes

--- server/config/db_connection.js
+++ server/config/db_connection.js
@@ -7,7 +7,7 @@ const connectionString = process.env.MONGODB_URI;
 mongoose.set('strictQuery', false);
 
 mongoose.connect(connectionString)
-  .then(() => console.log('Mongoose connected to the database'))
+  .then(() => console.log('Mongoose connected to the database - string changed'))
   .catch((err) => console.log(`Mongoose connection error: ${err}`));
 
 mongoose.connection.on('disconnected', () => {
